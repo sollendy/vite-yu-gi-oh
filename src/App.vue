12c937112ce7a2e7449f2b77b6251c7c8af513f6
sollendy <edlannino@tutanota.com>
2023-03-28 16:38:39 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script>
+
+</script>
+
+<template>
+  <AppMain></AppMain>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/components/AppMain.vue
@@ -0,0 +1,33 @@
+<script>
+    import {store} from "../store.js";
+    import axios from "axios";
+    import OggettoCard from "./oggettoCard.vue"
+    export default {
+        name: "AppMain",
+        data() {
+            return {
+                store
+            }
+        },
+        components: {
+            OggettoCard,
+        },
+        created() {
+            axios.get('https://db.ygoprodeck.com/api/v7/cardinfo.php?num=50&offset=0').then((res)=>{
+                this.store.yugiCards = res.data.name;
+                console.log(res.data.name);
+            })
+        }
+    }
+
+</script>
+
+<template>
+  <div id="lista-carte">
+    <OggettoCard v-for="carta in store.yugiCards"></OggettoCard>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
